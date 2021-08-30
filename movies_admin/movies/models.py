c26c74baa7a8cf6bf549bf82348e45708b058970
svvladimir-ru <svetlakov454@gmail.com>
2021-08-30 10:13:42 +0300

    добавил к полям created_created_at,

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class FilmWork(models.Model):
     """Признаюсь построил через python manage.py inspectdb > models.py
-    Добавил поля в genres, persons для выполнения задачи
+    Добавил поля в genres, persons для выполнения задачи, Добавил к полям created_created_at,
+    updated_at поля auto_now_add и auto_now
     """
     id = models.UUIDField(primary_key=True)
     title = models.TextField()
@@ -13,8 +14,8 @@ class FilmWork(models.Model):
     file_path = models.TextField(blank=True, null=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
     type = models.TextField()
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
     genres = models.ManyToManyField('Genre', through='GenreFilmWork')
     persons = models.ManyToManyField('Person', through='PersonFilmWork')
 
@@ -27,8 +28,8 @@ class Genre(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(unique=True, max_length=30)
     description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
 
     class Meta:
         managed = False
@@ -39,7 +40,7 @@ class GenreFilmWork(models.Model):
     id = models.UUIDField(primary_key=True)
     film_work = models.ForeignKey(FilmWork, models.DO_NOTHING)
     genre = models.ForeignKey(Genre, models.DO_NOTHING)
-    created_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     class Meta:
         managed = False
@@ -51,8 +52,8 @@ class Person(models.Model):
     id = models.UUIDField(primary_key=True)
     full_name = models.TextField()
     birth_date = models.DateField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
 
     class Meta:
         managed = False
@@ -64,7 +65,7 @@ class PersonFilmWork(models.Model):
     film_work = models.ForeignKey(FilmWork, models.DO_NOTHING)
     person = models.ForeignKey(Person, models.DO_NOTHING)
     role = models.TextField()  # This field type is a guess.
-    created_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     class Meta:
         managed = False
