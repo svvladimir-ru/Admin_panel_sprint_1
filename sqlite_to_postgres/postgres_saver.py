8e94e7ad02dfd91cfced001ff4a74889abb45979
svvladimir-ru <svetlakov454@gmail.com>
2021-08-31 09:23:12 +0300

    postgres_saver привел к лучшему и более рабочему виду

--- sqlite_to_postgres/postgres_saver.py
+++ sqlite_to_postgres/postgres_saver.py
@@ -4,8 +4,9 @@ from psycopg2.extensions import connection as _connection
 class PostgresSaver:
     """Класс для сохранения данных в бд
     Принимает словарь из sqllite_loader.py со значениями из dataclasses
-    проходит циклом, каждые 100 записей добавляет в postgres.
-    В postgres добавляются данные из методов, mogrify + соответствующие названия таблиц
+    проходит циклом, каждые 100 записей добавляет в postgres. Пришлось сделать дублирование кода,
+    что бы добавлять последние записи.
+    В postgres добавляются данные из методов, соответствующих названиям таблиц
     """
 
     def __init__(self, pg_conn: _connection):
@@ -23,34 +24,34 @@ class PostgresSaver:
 
     def mogrify_film_work(self, data) -> list:
         args = ','.join(self.cursor.mogrify(
-            "('{}', '{}', {}, {}, {}, '{}', '{}', '{}', {}, '{}')".format(
+            "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (
                 item.title.replace("'", '"'),
-                str(item.description).replace("'", '"') if item.description else 'null',
-                item.creation_date if item.file_path else 'null',
-                item.certificate if item.file_path else 'null',
-                item.file_path if item.file_path else 'null',
+                str(item.description).replace("'", '"') if item.description else None,
+                item.creation_date if item.file_path else None,
+                item.certificate if item.file_path else None,
+                item.file_path if item.file_path else None,
                 item.type,
                 item.created_at,
                 item.updated_at,
-                item.rating if item.rating else 'null',
+                item.rating if item.rating else None,
                 item.id
             )).decode() for item in data)
         return args
 
     def mogrify_genre(self, data) -> list:
         args = ','.join(self.cursor.mogrify(
-            "('{}', {}, '{}', '{}', '{}')".format(
-                item.name,
-                str(item.description).replace("'", '"').replace("None", 'null'),
+            "(%s, %s, %s, %s, %s)",
+            (item.name,
+                str(item.description).replace("'", '"') if item.description else None,
                 item.created_at,
                 item.updated_at,
                 item.id
-            )).decode() for item in data)
+             )).decode() for item in data)
         return args
 
     def mogrify_genre_film_work(self, data) -> list:
         args = ','.join(self.cursor.mogrify(
-            "('{}', '{}', '{}', '{}')".format(
+            "(%s, %s, %s, %s)", (
                 item.film_work_id,
                 item.genre_id,
                 item.created_at,
@@ -60,9 +61,9 @@ class PostgresSaver:
 
     def mogrify_person(self, data) -> list:
         args = ','.join(self.cursor.mogrify(
-            "('{}', {}, '{}', '{}', '{}')".format(
+            "(%s, %s, %s, %s, %s)", (
                 item.full_name.replace("'", '"'),
-                item.birth_date if item.birth_date else 'null',
+                item.birth_date if item.birth_date else None,
                 item.created_at,
                 item.updated_at,
                 item.id
@@ -71,7 +72,7 @@ class PostgresSaver:
 
     def mogrify_person_film_work(self, data) -> list:
         args = ','.join(self.cursor.mogrify(
-            "('{}', '{}', '{}', '{}', '{}')".format(
+            "(%s, %s, %s, %s, %s)", (
                 item.film_work_id,
                 item.person_id,
                 item.role,
@@ -80,7 +81,7 @@ class PostgresSaver:
             )).decode() for item in data)
         return args
 
-    def save_all_data(self, data: dict) -> bool:
+    def save_all_data(self, data: dict) -> bool:  # убрал try, except, так как rollback срабатывает автоматом.
         for table in data:
             method = self.methods[table]
             batch = 100
