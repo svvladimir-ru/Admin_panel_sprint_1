a884ca85f65aeded2b2c6cbf5dc396e30c92419c
svvladimir-ru <svetlakov454@gmail.com>
2021-08-31 12:05:51 +0300

    эталонное решение postgres_saver

--- sqlite_to_postgres/postgres_saver.py
+++ sqlite_to_postgres/postgres_saver.py
@@ -13,83 +13,26 @@ class PostgresSaver:
         self.conn = pg_conn
         self.cursor = self.conn.cursor()
         self.counter = 0
-        self.methods = {
-            'film_work': self.mogrify_film_work,
-            'genre': self.mogrify_genre,
-            'genre_film_work': self.mogrify_genre_film_work,
-            'person': self.mogrify_person,
-            'person_film_work': self.mogrify_person_film_work,
-        }
 
-    def mogrify_film_work(self, data) -> list:
-        args = ','.join(self.cursor.mogrify(
-            "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (
-                item.title,
-                item.description,
-                item.creation_date,
-                item.certificate,
-                item.file_path,
-                item.type,
-                item.created_at,
-                item.updated_at,
-                item.rating,
-                item.id
-            )).decode() for item in data)
-        return args
-
-    def mogrify_genre(self, data) -> list:
-        args = ','.join(self.cursor.mogrify(
-            "(%s, %s, %s, %s, %s)",
-            (item.name,
-                item.description,
-                item.created_at,
-                item.updated_at,
-                item.id
-             )).decode() for item in data)
-        return args
-
-    def mogrify_genre_film_work(self, data) -> list:
-        args = ','.join(self.cursor.mogrify(
-            "(%s, %s, %s, %s)", (
-                item.film_work_id,
-                item.genre_id,
-                item.created_at,
-                item.id
-            )).decode() for item in data)
-        return args
-
-    def mogrify_person(self, data) -> list:
-        args = ','.join(self.cursor.mogrify(
-            "(%s, %s, %s, %s, %s)", (
-                item.full_name,
-                item.birth_date,
-                item.created_at,
-                item.updated_at,
-                item.id
-            )).decode() for item in data)
-        return args
-
-    def mogrify_person_film_work(self, data) -> list:
-        args = ','.join(self.cursor.mogrify(
-            "(%s, %s, %s, %s, %s)", (
-                item.film_work_id,
-                item.person_id,
-                item.role,
-                item.created_at,
-                item.id,
-            )).decode() for item in data)
+    def mogrify_create(self, data) -> list:
+        ss = " ,%s"
+        args = ','.join(
+            self.cursor.mogrify(
+                f"(%s{ss * (len(data[0].__dict__) - 1)})",
+                list(value for _, value in item.__dict__.items())
+            ).decode() for item in data
+        )
         return args
 
     def save_all_data(self, data: dict) -> bool:
         for table in data:
-            method = self.methods[table]
             batch = 100
             for i in range(0, len(data[table]) + batch, batch):
                 data_save = data[table][i: i + batch]
                 if data_save:
                     self.cursor.execute(f"""
                         INSERT INTO content.{table} ({', '.join(i for i in data[table][0].__annotations__)})
-                        VALUES {method(data_save)}
+                        VALUES {self.mogrify_create(data_save)}
                         ON CONFLICT (id) DO NOTHING
                         """)
                     self.conn.commit()
